3a754366e7f6a484346fb4616eb36ce10a066126
Casper <156071110+CasperCloudku@users.noreply.github.com>
2024-03-11 04:07:42 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -7,14 +7,23 @@ const mount2 = document.querySelector('.mount2')
 
 document.addEventListener('scroll', function() {
     let value = window.scrollY
-    // console.log(value)
-    title.style.marginTop = value * 1.1 + 'px'
+    let time = new Date()
+    let hour = time.getHours()
 
+    // Mengubah efek saat scroll menjadi malam
+    if (hour >= 18 || hour <= 6) {
+        document.body.style.backgroundColor = '#000' // Ubah warna background menjadi hitam saat malam
+        // Tambahkan efek lain yang diinginkan saat malam
+    } else {
+        document.body.style.backgroundColor = '#fff' // Kembalikan warna background ke putih saat siang
+        // Hapus efek malam yang telah ditambahkan sebelumnya
+    }
+
+    // Implementasi perubahan elemen lain sesuai kebutuhan
+    title.style.marginTop = value * 1.1 + 'px'
     leaf1.style.marginLeft = -value + 'px'
     leaf2.style.marginLeft = value + 'px'
-
     bush2.style.marginBottom = -value + 'px'
-
     mount1.style.marginBottom = -value * 1.1 + 'px'
     mount2.style.marginBottom = -value * 1.2 + 'px'
 })
